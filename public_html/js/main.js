e3ce3cdc3321366dbbff528164b1eec927408f68
aNeilator <virtualrealityneil@gmail.com>
2016-08-15 23:54:55 +0100

    ui draggable

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -33,31 +33,102 @@ function populateData(){
       //     $('#stream-all').html(jsonQuery);
       // });
 
-      $.get( HOST+"ReceiveScores.php",
-        { TYPE: "leaderboard", DATERANGE: "year" },
-        function( data ) {
-          var asJson = JSON.parse(data);
-          console.log(asJson);
-          var jsonQuery = createTable(asJson);
-          $('#stream-players').html(jsonQuery);
+      // $.get( HOST+"ReceiveScores.php",
+      //   { TYPE: "leaderboard", DATERANGE: "year" },
+      //   function( data ) {
+      //     var asJson = JSON.parse(data);
+      //     console.log(asJson);
+      //     var jsonQuery = createTable(asJson);
+      //     $('#stream-players').html(jsonQuery);
           $('.name').draggable({
               revert:"valid",
               helper:"clone"
           });
-      });
+      // });
 
       $.get( HOST+"ReceiveScores.php",
         { TYPE: "player-all", DATERANGE: "year" },
         function( data ) {
           var asJson = JSON.parse(data);
           console.log(asJson);
-          $('.name').draggable({
-              revert:"valid",
-              helper:"clone"
+          var namedrags = $('.name');
+          namedrags.draggable({
+            start: function() {
+              $('.circle').css({opacity:1});
+            },
+            stop: function() {
+              // playerDrag.dropped++;
+              if (playerDrag.winner!=='' ||
+                  playerDrag.loser!==''){
+              }else{
+                $('.circle').css({opacity:0});
+              }
+            },
+            snap: ".circle",
+              revert:"valid"
+              // helper:function(event){
+              //   console.log($(this));
+              //   return '<div class="name-helper">I am a helper - drag me!</div>';
+              // },
           });
+          $('.circle').droppable( {
+            // accept: '.circle',
+            drop: clone
+          } );
+
+          function clone(event, ui) {
+              var draggable = ui.draggable;
+              var playerName =draggable[0]['innerHTML'];
+              $(this).html(playerName);
+              if ($(this).hasClass('left-circle')){
+                playerDrag.winner = playerName;
+              }else{
+                playerDrag.loser = playerName;
+              }
+          }
           // var jsonQuery = createTable(asJson);
           // $('#stream-players').html(jsonQuery);
       });
+}
+
+var playerDrag = {
+  "winner":'',
+  "loser":'',
+  "dropped":0
+}
+
+function handleCardDrop( event, ui ) {
+  var slotNumber = $(this).data( 'number' );
+  var cardNumber = ui.draggable.data( 'number' );
+
+  // If the card was dropped to the correct slot,
+  // change the card colour, position it directly
+  // on top of the slot, and prevent it being dragged
+  // again
+
+  if ( slotNumber == cardNumber ) {
+    ui.draggable.addClass( 'correct' );
+    ui.draggable.draggable( 'disable' );
+    $(this).droppable( 'disable' );
+    ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+    ui.draggable.draggable( 'option', 'revert', false );
+    correctCards++;
+  }
+
+  // If all the cards have been placed correctly then display a message
+  // and reset the cards for another go
+
+  if ( correctCards == 10 ) {
+    $('#successMessage').show();
+    $('#successMessage').animate( {
+      left: '380px',
+      top: '200px',
+      width: '400px',
+      height: '100px',
+      opacity: 1
+    } );
+  }
+
 }
 
 function createTable(mydata) {
