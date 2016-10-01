a2b670499ae2a6399a603acbe0754fbeb6e56e3a
aNeilator <virtualrealityneil@gmail.com>
2016-10-01 14:35:52 +0100

    mac switch

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -5,8 +5,6 @@ $(document).ready(function() {
 
 
 var HOST_PHP = "http://pool88.site88.net/php/";
-var tableUpdateNum = 0;
-var namedrags;
 var GPLAYERS = {
     "winner": '',
     "loser": ''
@@ -17,23 +15,9 @@ function initialise() {
 
     $('.circle').droppable({
         // accept: '.circle',
-        drop: function(event, ui) {
-
-            var draggable = ui.draggable[0];
-            var playerName = draggable['innerHTML'];
-
-            if ($(this).hasClass('left-circle')) {
-                GAME.setNameChosen(true, true, draggable['htmlFor'], playerName);
-            } else if ($(this).hasClass('right-circle')) {
-                GAME.setNameChosen(false, true, draggable['htmlFor'], playerName);
-            }
-
-        },
+        drop: GAME.handleCardDrop,
         hoverClass: 'hovered-blink'
     });
-
-    // });
-
 
     $('#cancel-right').click(function() {
         GAME.setNameChosen(false, false, GPLAYERS.loser, '');
@@ -49,6 +33,11 @@ function initialise() {
 
     $('#add-uid').on('change keyup paste', function() {
         var value = $(this).val();
+		if (value.trim().length==0){
+			$('#button-add-player').prop( "disabled", true );
+		}else{
+			$('#button-add-player').prop( "disabled", false );
+		}
         var flname = value.split('.');
         console.log(flname);
         $('#add-fname').val(flname[0].capitalizeFirstLetter());
@@ -62,8 +51,9 @@ function pullLeaderboard() {
 }
 
 var GAME = {
+
     setDraggables: function() {
-        namedrags.draggable({
+        UI.namedrags.draggable({
             start: function() {
                 $('.circle').css({
                     opacity: 1
@@ -74,7 +64,7 @@ var GAME = {
             },
             stop: function() {
 
-                var uid = $(this)[0]['htmlFor'];
+                var uid = $(this).children(":first").attr('for');
                 var curr = $(this);
                 GAME.resetNameDrag(uid, curr);
             },
@@ -83,21 +73,53 @@ var GAME = {
             revert: "invalid",
             helper: "clone"
         });
-        namedrags.draggable("enable");
+        UI.namedrags.draggable("enable");
+    },
+
+    setNameChosen: function(isWinner, isChosen, uid, playerName) {
+        if (isChosen) {
+            if (isWinner) {
+				if (GPLAYERS.winner!==''){
+					var ll = $("label[for='" + GPLAYERS.winner + "']").parent();
+					GAME.resetNameDrag(uid, ll);
+				}
+                GPLAYERS.winner = uid;
+                $('#add-name-left').text(playerName);
+            } else {
+				if (GPLAYERS.loser!==''){
+					var ll = $("label[for='" + GPLAYERS.loser + "']").parent();
+					GAME.resetNameDrag(uid, ll);
+				}
+                GPLAYERS.loser = uid;
+                $('#add-name-right').text(playerName);
+            }
+        } else {
+			var ll = $("label[for='" + uid + "']").parent();
+
+            if (isWinner) {
+                GPLAYERS.winner = '';
+                $('#add-name-left').text('');
+            } else {
+                $('#add-name-right').text('');
+                GPLAYERS.loser = '';
+
+            }
+            GAME.resetNameDrag(uid, ll);
+        }
     },
 
     resetNameDrag: function(uid, curr) {
 
         //both added
         if (GPLAYERS.winner !== '' && GPLAYERS.loser !== '') {
-            namedrags.draggable("disable");
+            // UI.namedrags.draggable("disable");
             $('.button-add button').css({
                 opacity: 1
             });
         }
         //one of them added
         else if (GPLAYERS.winner !== '' || GPLAYERS.loser !== '') {
-            namedrags.draggable("enable");
+            UI.namedrags.draggable("enable");
         }
         //both of them empty
         else if (GPLAYERS.winner === '' && GPLAYERS.loser === '') {
@@ -117,70 +139,25 @@ var GAME = {
             curr.draggable("enable");
         }
     },
-    setNameChosen: function(isWinner, isChosen, uid, playerName) {
-        if (isChosen) {
-            if (isWinner) {
-                GPLAYERS.winner = uid;
-                $('#add-name-left').text(playerName);
-            } else {
-                GPLAYERS.loser = uid;
-                $('#add-name-right').text(playerName);
-            }
-        } else {
-            var ll = $("label[for='" + uid + "']");
-
-            if (isWinner) {
-                GPLAYERS.winner = '';
-                $('#add-name-left').text('');
-            } else {
-                $('#add-name-right').text('');
-                GPLAYERS.loser = '';
-
-            }
-            GAME.resetNameDrag(uid, ll);
-        }
-    },
 
     handleCardDrop: function(event, ui) {
-        var slotNumber = $(this).data('number');
-        var cardNumber = ui.draggable.data('number');
 
-        // If the card was dropped to the correct slot,
-        // change the card colour, position it directly
-        // on top of the slot, and prevent it being dragged
-        // again
+		var draggable = ui.draggable.children(":first");
+		var playerName = draggable.html();
+		var playerUID = draggable.attr('for');
 
-        if (slotNumber == cardNumber) {
-            ui.draggable.addClass('correct');
-            ui.draggable.draggable('disable');
-            $(this).droppable('disable');
-            ui.draggable.position({
-                of: $(this),
-                my: 'left top',
-                at: 'left top'
-            });
-            ui.draggable.draggable('option', 'revert', false);
-            correctCards++;
-        }
+		if ($(this).hasClass('left-circle')) {
+			GAME.setNameChosen(true, true, playerUID, playerName);
+		} else if ($(this).hasClass('right-circle')) {
+			GAME.setNameChosen(false, true, playerUID, playerName);
+		}
 
-        // If all the cards have been placed correctly then display a message
-        // and reset the cards for another go
-
-        if (correctCards == 10) {
-            $('#successMessage').show();
-            $('#successMessage').animate({
-                left: '380px',
-                top: '200px',
-                width: '400px',
-                height: '100px',
-                opacity: 1
-            });
-        }
-
-    }
+	}
 }
 
 var UI = {
+
+	namedrags: $('.name'),
 
     loaderToggle: function(show) {
         var loader = $('#loader');
@@ -195,56 +172,69 @@ var UI = {
         php.addPlayer();
     },
 
-	addPlayerViewToggle: function() {
-	    $('.add-player-overlay').toggle();
-	}
-	,
+    addPlayerViewToggle: function() {
+        $('.add-player-overlay').toggle();
+    },
 
-	createTable: function(mydata) {
-	    $('#stream-players').html('<table id="leader-table" class="leaderboard " border=0>');
-	    var table = $('#leader-table');
-	    var tblHeader = "<thead><tr>";
-	    for (var k in mydata[0]) {
-	        if (k === "ID") continue;
-	        tblHeader += "<th>" + k + "</th>";
-	    }
-	    tblHeader += "</tr></thead>";
-	    $(tblHeader).appendTo(table);
-	    var setNewPlayerNow = false;
-	    var newPPos = 0;
-	    $.each(mydata, function(index, value1) {
-	        var TableRow = "<tr>";
-	        $.each(value1, function(key, val) {
-	            if (key === "ID") return true;
-	            if (key === "NAME")
-	                TableRow += "<td><label class='name' for='" +
-	                value1.ID + "'>" + val + "</label></td>";
-	            else if (key === "WON")
-	                TableRow += "<td class='num'>" + val + "</td>";
-	            else if (key === "LOST")
-	                TableRow += "<td class='num'>" + val + "</td>";
-	            else if (key === "SCORE") {
-	                if (val < newPPos && setNewPlayerNow == false) {
-	                    setNewPlayerNow = true;
-	                    newPPos = -100000;
-	                }
-	                TableRow += "<td>" + val + "</td>";
-	            } else {
-	                TableRow += "<td>" + val + "</td>";
-	            }
-	        });
-	        if (setNewPlayerNow) {
-	            var str = "<td> </td><td> Add new Player </td><td></td><td></td><td></td>";
-	            // table.append("<tr onclick='UI.addPlayerViewToggle()' class='add-player'>"+str+"</tr>");
-	            setNewPlayerNow = false;
-	        }
-	        TableRow += "</tr>";
-	        table.append(TableRow);
-	    });
-	    // table.tableSort();
-	    // return (table));
-	}
-
+    createTable: function(mydata) {
+        $('#stream-players').html('<table id="leader-table" class="leaderboard " border=0>');
+        var table = $('#leader-table');
+        var tblHeader = "<thead><tr>";
+        for (var k in mydata[0]) {
+            if (k === "ID") continue;
+            else if (k === "NAME") {
+				tblHeader += "<th style='text-align:left;'>" + k + "</th>";
+			}
+            else {
+				tblHeader += "<th>" + k + "</th>";
+			}
+        }
+        tblHeader += "</tr></thead>";
+        $(tblHeader).appendTo(table);
+        var setNewPlayerNow = false;
+        var newPPos = 0;
+        $.each(mydata, function(index, value1) {
+            var TableRow = "<tr class=''>";
+            $.each(value1, function(key, val) {
+                if (key === "ID") return true;
+                if (key === "NAME"){
+					var name = val;
+					var LIM = 16;
+					if (name.length>LIM) {
+						name = name.substr(0,LIM)+'...';
+					}
+                    TableRow += "<td style='text-align:left;' class='name'><label for='" +
+                     value1.ID + "'>" + name + "</label></td>";
+				}
+                else if (key === "WON")
+                    TableRow += "<td class='num'>" + val + "</td>";
+                else if (key === "LOST")
+                    TableRow += "<td class='num'>" + val + "</td>";
+                else if (key === "SCORE") {
+                    if (val < newPPos && setNewPlayerNow == false) {
+                        setNewPlayerNow = true;
+                        newPPos = -100000;
+                    }
+					if (val>0){
+						TableRow += "<td class='score red'>" + val + "</td>";
+					}else{
+						TableRow += "<td class='score blue'>" + val + "</td>";
+					}
+                } else {
+                    TableRow += "<td>" + val + "</td>";
+                }
+            });
+            if (setNewPlayerNow) {
+                var str = "<td> </td><td> Add new Player </td><td></td><td></td><td></td>";
+                // table.append("<tr onclick='UI.addPlayerViewToggle()' class='add-player'>"+str+"</tr>");
+                setNewPlayerNow = false;
+            }
+            TableRow += "</tr>";
+            table.append(TableRow);
+        });
+        // table.tableSort();
+        // return (table));
+    }
 
 }
 
@@ -294,15 +284,13 @@ var php = {
                 $('#leader-table').tableSort({
                     sortBy: ['numeric', 'text', 'numeric', 'numeric', 'numeric']
                 });
-                if (tableUpdateNum == 0) {}
                 UI.loaderToggle(false);
-                namedrags = $('.name');
+                UI.namedrags = $('.name');
                 GAME.setDraggables();
             });
     }
 
 }
-
 
 String.prototype.capitalizeFirstLetter = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
